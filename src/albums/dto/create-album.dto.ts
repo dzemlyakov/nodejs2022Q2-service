ef1175ebf1ff9ad2fdda0ee95d9575f5f8b48f42
Дмитрий Земляков <dmitriyz@Mac-mini-Dmitrij.local>
2022-07-14 16:24:30 +0300

    feat: complete validation albums

--- src/albums/dto/create-album.dto.ts
+++ src/albums/dto/create-album.dto.ts
@@ -1,5 +1,22 @@
+import {
+  IsInt,
+  IsNotEmpty,
+  IsString,
+  IsUUID,
+  ValidateIf,
+} from 'class-validator';
+import { isNull } from 'lodash';
+
 export class CreateAlbumDto {
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsInt()
+  @IsNotEmpty()
   year: number;
+
+  @ValidateIf((_, value) => !isNull(value))
+  @IsUUID('4')
   artistId: string | null;
 }
--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,16 @@ import { dirname, join } from 'path';
 import { readFile } from 'fs/promises';
 import { parse } from 'yaml';
 import { SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const rootDirname = dirname(__dirname);
   const DOC_API = await readFile(join(rootDirname, 'doc', 'api.yaml'), 'utf-8');
   const document = parse(DOC_API);
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+  );
 
   SwaggerModule.setup('doc', app, document);
   await app.listen(4000);

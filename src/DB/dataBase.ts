f8f275584989bf901882a850d2a5eb8e432a2209
Дмитрий Земляков <dmitriyz@Mac-mini-Dmitrij.local>
2022-07-15 08:52:04 +0300

    feat: complete users module

--- src/DB/dataBase.ts
+++ src/DB/dataBase.ts
@@ -2,12 +2,14 @@ import { Injectable } from '@nestjs/common';
 import { Album } from 'src/albums/entities/album.entity';
 import { Artist } from 'src/artists/entities/artist.entity';
 import { Track } from 'src/tracks/entities/track.entity';
+import { User } from 'src/users/entities/user.entity';
 
 @Injectable()
 export class InMemoryDB {
   albums: Album[] = [];
   tracks: Track[] = [];
   artists: Artist[] = [];
+  users: User[] = [];
 }
 
 export default InMemoryDB;
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { Module } from '@nestjs/common';
 import { AlbumsModule } from './albums/albums.module';
 import { TracksModule } from './tracks/tracks.module';
 import { ArtistsModule } from './artists/artists.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [AlbumsModule, TracksModule, ArtistsModule],
+  imports: [AlbumsModule, TracksModule, ArtistsModule, UsersModule],
 })
 export class AppModule {}
